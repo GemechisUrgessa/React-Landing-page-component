8c66e3330be0af08642daf311c4d80c391cde9a8
GemechisUrgessa <gemechisurgessa@gmail.com>
2022-12-12 12:22:07 +0300

    home page animation added

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -1,10 +1,57 @@
 import NavBar from "./navBar";
+import '../stylesheets/homePage.css';
+import Particles from "react-tsparticles";
 
 const HomePage = (): JSX.Element => {
+
     return (
         <header>
-            <NavBar />
-
+            {/* <NavBar /> */}
+            <Particles
+                params={{
+                    particles: {
+                        number: {
+                            value: 160,
+                            density: {
+                                enable: true,
+                                value_area: 1500
+                            }
+                        },
+                        line_linked: {
+                            enable: false,
+                            opacity: 0.03
+                        },
+                        move: {
+                            direction: "right",
+                            speed: 0.05
+                        },
+                        size: {
+                            value: 1
+                        },
+                        opacity: {
+                            anim: {
+                                enable: true,
+                                speed: 1,
+                                opacity_min: 0.05
+                            }
+                        }
+                    },
+                    interactivity: {
+                        events: {
+                            onclick: {
+                                enable: true,
+                                mode: "push"
+                            }
+                        },
+                        modes: {
+                            push: {
+                                particles_nb: 1
+                            }
+                        }
+                    },
+                    retina_detect: true
+                }}
+            />
         </header>
     )
 }
